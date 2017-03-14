00675abaaebc2d4b2bdbb9877d508d262ce545f3
Hugo BARROUX <Erondror@users.noreply.github.com>
2017-03-14 09:44:18 +0100

    Update docker_compose.sh

--- docker_compose.sh
+++ docker_compose.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-@echo off
 
+
+echo "---------------------------------------------------------------"
 echo "Installation de docker-compose en cours ..."
+echo "---------------------------------------------------------------"
 
 mkdir -p /opt/bin
 curl -L `curl -s https://api.github.com/repos/docker/compose/releases/latest | jq -r '.assets[].browser_download_url | select(contains("Linux") and contains("x86_64"))'` > /opt/bin/docker-compose
